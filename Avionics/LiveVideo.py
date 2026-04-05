373b5d8c05de762c73de6ba403d18a2b1cd5b6e6
Gameknight77YT <82116806+Gameknight77YT@users.noreply.github.com>
2026-04-04 19:19:15 -0500

    oops forgot the import

--- Avionics/LiveVideo.py
+++ Avionics/LiveVideo.py
@@ -1,7 +1,7 @@
 import socket
 import time
 from picamera2 import Picamera2
-from picamera2.encoders import MjpegEncoder, H264Encoder
+from picamera2.encoders import MJPEGEncoder, H264Encoder
 from picamera2.outputs import FileOutput
 
 # ==========================================
