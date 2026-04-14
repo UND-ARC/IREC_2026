ffcee09dd4bd1c65f138f55e5eeff3a99e753ff8
Gameknight77YT <82116806+Gameknight77YT@users.noreply.github.com>
2026-04-14 13:12:38 -0500

    pycharm did an oops with an import

--- Avionics/LiveVideo.py
+++ Avionics/LiveVideo.py
@@ -4,7 +4,6 @@ import cv2
 import numpy as np
 import io
 
-from black import output
 from cv2.datasets import none
 from picamera2 import Picamera2, MappedArray
 from picamera2.encoders import H264Encoder
