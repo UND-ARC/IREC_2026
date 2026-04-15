4162f612a31244f336d850c445e503550fb1a6a8
Gameknight77YT <82116806+Gameknight77YT@users.noreply.github.com>
2026-04-15 14:43:11 -0500

    tuned more stuff

--- Avionics/TX_GNU_radio.py
+++ Avionics/TX_GNU_radio.py
@@ -45,7 +45,6 @@ class TX_GNU_radio(gr.top_block):
         ##################################################
 
         self.network_udp_source_0 = network.udp_source(gr.sizeof_char, 1, 9000, 0, 1316, True, False, False)
-        self.network_udp_source_0.set_min_output_buffer(131072)
         self.iio_pluto_sink_0 = iio.fmcomms2_sink_fc32('192.168.3.1' if '192.168.3.1' else iio.get_pluto_uri(), [True, True], 131072, False)
         self.iio_pluto_sink_0.set_len_tag_key('')
         self.iio_pluto_sink_0.set_bandwidth(20000000)
