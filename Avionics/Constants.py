d629d98203b865c5f61c1fd5e35f622495d78d34
Gameknight77YT <82116806+Gameknight77YT@users.noreply.github.com>
2026-04-16 15:30:30 -0500

    lowered bitrate

--- Avionics/Constants.py
+++ Avionics/Constants.py
@@ -11,8 +11,7 @@ IS_FLIGHT_MODE = True   # Set TRUE for RF, Set false for Ethernet to laptop
 USE_OVERLAY    = True    # Set TRUE to draw telemetry overlay
 
 if IS_FLIGHT_MODE:
-    # BITRATE is the actual video payload (leaves room for shaking)
-    BITRATE = 700_000
+    BITRATE = 500_000
     IDR_VAL = 10
 else:
     BITRATE = 3_000_000
