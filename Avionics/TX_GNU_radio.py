cc387ed680a5371cbaa3ca6da434e189609646b7
Gameknight77YT <82116806+Gameknight77YT@users.noreply.github.com>
2026-04-16 13:13:30 -0500

    packet size

--- Avionics/TX_GNU_radio.py
+++ Avionics/TX_GNU_radio.py
@@ -45,7 +45,7 @@ class TX_GNU_radio(gr.top_block):
         # Blocks
         ##################################################
 
-        self.network_udp_source_0 = network.udp_source(gr.sizeof_char, 1, 9000, 0, 1316, True, False, False)
+        self.network_udp_source_0 = network.udp_source(gr.sizeof_char, 1, 9000, 0, 188, True, False, False)
         self.iio_pluto_sink_0 = iio.fmcomms2_sink_fc32('ip:192.168.3.1' if 'ip:192.168.3.1' else iio.get_pluto_uri(), [True, True], 131072, False)
         self.iio_pluto_sink_0.set_len_tag_key('')
         self.iio_pluto_sink_0.set_bandwidth(20000000)
