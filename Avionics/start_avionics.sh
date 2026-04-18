6fa827131803358bf4847c0b8ed45f3cbcc82a72
Gameknight77YT <82116806+Gameknight77YT@users.noreply.github.com>
2026-04-18 14:43:32 -0500

    swaped commands

--- Avionics/start_avionics.sh
+++ Avionics/start_avionics.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # The -u flag forces Python to write logs instantly, saving them during a crash
+python3 -u /home/arc/Github/IREC_2026/Avionics/LiveVideo.py &
+
 python3 -u /home/arc/Github/IREC_2026/Avionics/TX_GNU_radio.py &
 
-python3 -u /home/arc/Github/IREC_2026/Avionics/LiveVideo.py &
+
 
 # Wait keeps the script running as long as the background tasks are running
 wait
